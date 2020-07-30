95d500f37d1d957f6730ebf486c8fa53b7d2fb65
deepak <deepakjha6210@gmail.com>
2020-07-30 18:39:50 +0530

    adding the practice problem 3 solution

---
+++ 03_practice.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    float celsius = 37, far;
+    far = (celsius* 9/5) + 32;
+    printf("The value of this celcius tempratue in Fahrenheit is %f", far);
+    return 0;
+
+}
