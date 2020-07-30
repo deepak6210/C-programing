b57ad71955b35445347de2e71aa236208a96d2f3
deepak <deepakjha6210@gmail.com>
2020-07-30 17:36:14 +0530

    renaming first.c as 1first.c and variable.c as 2variable.c and deleting the exe file placed by mistake and adding the 3input.c file

---
+++ 3input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter the value of a\n");
+    // Taking the value of a from user
+    scanf("%d", &a);
+    printf("Enter the value of b\n");
+    // Taking the value of b from the user 
+    scanf("%d", &b);
+    // Adding both this value 
+    printf("Sum of a and b is %d", a + b);
+    return 0;
+}
+// To get the Output run this proggram on your c compiler 
