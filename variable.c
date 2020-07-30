8ef503eaeb5745b05947af14872e42e631f9fd6e
deepak <deepakjha6210@gmail.com>
2020-07-30 16:49:39 +0530

    adding the varaible.c file

---
+++ variable.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/* this is multiline 
+comment as u studies 
+above 
+*/
+// Hello i am single line comment
+int main()
+{
+    int a = 4;
+    float b = 8.5;
+    char c = 'u';
+    int d = 23;
+    printf("The value of a is  %d \n", a);
+    printf("The value of b is  %f\n", b);
+    printf("The value of c is %c\n", c);
+    printf("Sum of a and d is %d\n", a + d);
+    return 0;
+}
+// \n = basically it is a new line character use to introduce new line.
+
+// Try at yourself write a programe to add two number's in c language 
+#include<stdio.h>
+
+int main()
+{
+    int first_num =  3;
+    int second_num = 5;
+    // writing a program to add to number 
+    printf("sum of the two number is %d\n", first_num + second_num);
+    return 0;
+}
