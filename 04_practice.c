9ede6d0bebf60201d1c88a80de590f696b4a591e
deepak <deepakjha6210@gmail.com>
2020-07-30 18:53:05 +0530

    adding the solution of 4th practice problem

---
+++ 04_practice.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    int principal = 100, rate = 4 , years = 1;
+    int simpleintrest = (principal*rate *years)/100;
+    printf("The value of simple interest %d", simpleintrest);
+    return 0;
+
+}
