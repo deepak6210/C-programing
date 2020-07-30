9e16ba84c9242f6482a1c7aab12a196c19e9ecfd
deepak <deepakjha6210@gmail.com>
2020-07-30 18:10:56 +0530

    adding first practice problem part a and b

---
+++ 01_practice_b.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int length,breadth;
+    printf("What is the length of rectange\n");
+    scanf("%d", &length);
+    printf("What is the breadth of rectange\n");
+    scanf("%d", &breadth);
+    printf("The area of your rectangle %d",length*breadth);
+    return 0;
+
+}
