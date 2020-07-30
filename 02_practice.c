b10f39b9c500ccec98c92d82929d06b3fbc93eb2
deepak <deepakjha6210@gmail.com>
2020-07-30 18:26:43 +0530

    adding the solution of practice problem 2

---
+++ 02_practice.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int radius = 3;
+    float pi = 3.14;
+    printf("The area of this cirle is %f\n", pi*radius*radius);
+    int height = 3;
+    printf("Volume of the cylinder is %f\n", pi*radius*radius*height);
+    return 0;
+}
