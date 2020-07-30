0b685686a82478135b0e0ca0b1f67c696eb6c911
deepak <deepakjha6210@gmail.com>
2020-07-30 21:16:09 +0530

    adding the type converdionn instruction file

---
+++ 03_Type_conversion_instruction.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("The value of 6 + 5.6 is %d\n", 6 + 5);
+    printf("The value of 6 + 5.6 is %f\n", 6 + 5.6);
+    printf("The value of 6.4 + 5.6 is %f\n", 6.4 + 5.6);
+    printf("The value of 5/2 is %d\n", 5/2);
+    printf("The value of 2/5 is %d\n", 2/5);
+    printf("The value of 5.0/2 is %f\n", 5.0/2);
+    printf("The value of 2.0/5 is %f\n", 2.0/5);
+    return 0;
+
+}
