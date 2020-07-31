9239e8f0d71b7bb92dc354482bb18757cfb98de9
deepak <deepakjha6210@gmail.com>
2020-07-31 13:43:36 +0530

    adding a program to check wheather the number is even or od using if else statement chapter 3

---
+++ 03_if_basic.c
@@ -0,0 +1,20 @@
+// c program to check wheather the number is even or odd 
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter a number \n");
+    scanf("%d", &a);
+
+    if (a % 2 == 0)
+    {
+        printf("%d is even\n", a);
+    }
+    else
+    {
+        printf("%d is odd\n", a);
+    }
+
+    return 0;
+}
