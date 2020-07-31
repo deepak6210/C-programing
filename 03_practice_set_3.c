8ae3f7c8545cf280f8851aa0c14c4ac3849430b8
deepak <deepakjha6210@gmail.com>
2020-07-31 21:17:25 +0530

    adding a practice problem of chapter 3 of incometax

---
+++ 03_practice_set_3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    float tax = 0 ,  income;
+    printf("Enter your  Income\n");
+    scanf("%f" , &income);
+    if (income>= 250000 && income<=500000){
+        tax = tax + 0.05*(income - 250000);
+    } 
+    if (income>= 500000 && income<=1000000){
+        tax = tax + 0.20*(income - 500000);
+    } 
+    if (income>= 1000000 ){
+        tax = tax + 0.30*(income - 250000);
+    } 
+    printf("your net income tax to be paid is %f\n", tax);
+    return 0;
+
+}
