8b9493f257466bd8c88c9508ce8ec991fb072172
deepak <deepakjha6210@gmail.com>
2020-07-31 10:30:16 +0530

    adding the operator precendene file

---
+++ 02_operator_precedence.c
@@ -0,0 +1,14 @@
+// you need to run this code on the compiler to get the output intentially i am not adding it  
+#include<stdio.h>
+int main()
+{
+    int x = 2;
+    int y = 3;
+
+    printf("The value of 3*x- 8*y is %d \n", 3 * x - 8 * y);
+    printf("The value of 3*y / 8*x is %d \n", 8 * y / 3 * x);
+    /*if the two operators with the same precedence come in single expession then it follow the 
+    left to right associativity it don't follow the genral rules of mathematics like boad mass*/  
+        return 0;
+
+}
