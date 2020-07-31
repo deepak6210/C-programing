d19d3339708298b04456f7f535309a43ebb54150
deepak <deepakjha6210@gmail.com>
2020-07-31 20:52:31 +0530

    adding practice problem of chapter 3

---
+++ 03_practice_set_2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int physics, chemistry, maths;
+    float total;
+    printf("Enter physics marks\n");
+    scanf("%d", &physics);
+
+    printf("Enter Chemsitry marks\n");
+    scanf("%d", &chemistry);
+
+    printf("Enter maths marks\n");
+    scanf("%d", &maths);
+    total = (physics + chemistry + maths) / 3;
+    if ((total < 40) || physics > 33 || chemistry > 33 || maths > 33)
+    {
+        printf("Your total percentage is %f and you are fail\n", total);
+    }
+    else
+    {
+        printf("your total percantage is %f and you are pass\n", total);
+    }
+
+    return 0;
+}
