8e9e9c5f284cb140e23e31695d4c619fac944e00
deepak <deepakjha6210@gmail.com>
2020-07-31 21:50:43 +0530

    addin practice problem of chapter 3 of finding a leap year

---
+++ 03_practice_problem_4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int leap_year;
+    float leap1, leap2, leap3;
+    printf("Enter the leap year\n");
+    scanf("%d", &leap_year);
+    leap1 = leap_year % 4;
+    if (leap1 == 0 )
+    {
+        printf(" %d  is a leap year\n", leap_year);
+    }
+    else
+    {
+        printf("It is not a leap year");
+    }
+
+    return 0;
+}
