16a9d7a39b9de0782ac9b15a77bd53c13eb7e790
deepak <deepakjha6210@gmail.com>
2020-07-31 11:44:01 +0530

    Adding the 2nd practice problem of chapter 2

---
+++ 02_practice_set_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter the number\n");
+    scanf("%d", &num);
+    printf("Divisibility test returns Reminder: %d\n", num % 97);
+
+    return 0;
+}
