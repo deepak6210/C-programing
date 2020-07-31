3b2dab9885edb7e3290010eb1e629fe0af1b938d
deepak <deepakjha6210@gmail.com>
2020-07-31 10:59:34 +0530

    adding the practice problem of 1st question from the seound chapter

---
+++ 02_practice_set_1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    // int a ; b = a; // it is invalid becuse b is not defined by us to the compiler 
+    int v = 3^3;  // this is bitwise zor operator we don't get the power of the but yes it is valid 
+    printf("this is valid this 3^3 %d\n", v);
+    // char dt = '21 dec 2020'; // this is not valid because char contain a space of single digit so we get error 
+    return 0;
+
+}
