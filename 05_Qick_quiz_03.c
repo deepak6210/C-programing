6d4fb98730275408ddbbc2ed50b9a5f7f056bf6c
deepak <deepakjha6210@gmail.com>
2020-08-05 00:25:49 +0530

    Adding the quick quizz of chapter 5

---
+++ 05_Qick_quiz_03.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int side;
+    printf("Enter the value of the side\n");
+    scanf("%d", &side);
+    printf("The value of the area is %f",pow(side,2));
+
+    return 0;
+
+}
