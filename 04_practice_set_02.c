3d828e0d967e00464605c64a4f39e495d2f4e490
deepak <deepakjha6210@gmail.com>
2020-08-04 09:43:48 +0530

    Adding the 04_practice_set_02.c

---
+++ 04_practice_set_02.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int n = 10 ; 
+    for (int i = 10; i; i--)
+    {
+       printf("The Table of the %d*%d =%d\n",n,i,n*i );
+    }
+    
+        return 0;
+
+}
