1850e9ad09ae57db0330d41560003aa9cef98b7c
deepak <deepakjha6210@gmail.com>
2020-08-06 14:52:18 +0530

    customization in call by value file

--- 06_callByValue_02.c
+++ 06_callByValue_02.c
@@ -2,10 +2,10 @@
 int sum(int a, int b);
 int main()
 {
-    int a =4, b=7;
-    printf("The value of a and b is %d and %d\n", a,b);
-    printf("The value of 4+7 is %d\n", sum(a,b));
-    printf("The value of a and b after function call is %d and %d\n", a,b);
+    int x=4, y=7;
+    printf("The value of a and b is %d and %d\n", x,y);
+    printf("The value of 4+7 is %d\n", sum(x,y));
+    printf("The value of a and b after function call is %d and %d\n", x,y);
     return 0;
 }
 int sum(int a, int b){
