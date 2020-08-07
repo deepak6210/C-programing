6ac47d59efa85b2c10cb29ae51b6282b4af1bbe7
deepak <deepakjha6210@gmail.com>
2020-08-07 15:45:16 +0530

    adding the synatx of array

---
+++ 07_arrays _basic_01.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    // naive way to create 4 ints
+    // int marks1, marks2 , marks3 , marks4;
+    // mark1 = 34; 
+    // mark2 = 45; 
+    // mark3 = 67; 
+    // mark4 = 87; 
+    int marks[4];
+    marks[0] = 34; 
+    marks[1] = 45;
+    marks[2] = 34;
+    marks[3] = 67;
+
+    return 0;
+
+}
