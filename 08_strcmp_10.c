db522f95c78f1b55b299f0bb603834d5f3671667
deepak <deepakjha6210@gmail.com>
2020-08-10 02:11:23 +0530

    adding the strcmp libraray function file

---
+++ 08_strcmp_10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    
+    char st1[45] = "Hello";
+    char *st2 = "Deepak";
+    int val = strcmp(st1, st2);
+    printf("Now the val is %d", val);
+    return 0;
+
+}
