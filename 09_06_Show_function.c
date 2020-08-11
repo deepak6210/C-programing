692da912d7c772a6bd6a6e48e5986d92a9ae7170
deepak <deepakjha6210@gmail.com>
2020-08-11 16:33:30 +0530

    passing the value of structure through a function

---
+++ 09_06_Show_function.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+struct employee
+{
+  int code;
+  float salary;
+  char name[10];  
+};
+
+void show(struct employee emp)
+{
+   printf("The code of employee is :%d\n", emp.code);    
+   printf("The salary of employee is :%f\n", emp.salary);    
+   printf("The name of employee is :%s\n", emp.name);    
+}
+
+int main()
+{
+    struct  employee e1;
+    struct employee *ptr;
+    
+    ptr = &e1;
+    // (*ptr).code = 101; // or we can also write: ptr->code = 101;
+    ptr->code = 101;
+    ptr->salary = 101.34;
+    strcpy(ptr->name, "Deepak");
+    show(e1);
+    return 0;
+}
